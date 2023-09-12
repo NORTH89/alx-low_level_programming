3e0ca54a68b20904830d6963056bc3ebeb9fb9c2
NOUREDDINE <nordintata0@gmail.com>
2023-09-12 15:09:22 -0700

    Functions nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_to_98 - from n to 98
  * @n: integer
  *
  * Return: this function print from n to 98
  */
-
 void print_to_98(int n)
 {
 	if (n < 98)
