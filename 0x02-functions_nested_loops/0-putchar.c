91bda956e16282a5e26691035e7b5592ff23d5b9
NOUREDDINE <nordintata0@gmail.com>
2023-09-12 05:39:30 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include "_putchar.h"
-
+#include <main.h>
 /**
  * main - entry point
  *
@@ -8,14 +7,14 @@
 
 int main(void)
 {
-	char l[] = {"_putchar"};
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(l[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
