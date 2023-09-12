2da9bfba568d42d534c4229752cc5bb117aba4a5
NOUREDDINE <nordintata0@gmail.com>
2023-09-12 04:41:14 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,24 +13,18 @@ int main()
 	for (i = 0; i <= 99; i++)
 	{
 		for (j = i; j <= 99; j++)
-		{
-
-			putchar('0' + (i / 10));
-
-			putchar('0' + (i % 10));
-
-			putchar(' ');
-
-			putchar('0' + (j / 10));
-
-			putchar('0' + (j % 10));
-
-			if (i != 99 || j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	{
+		putchar('0' + (i / 10));
+		putchar('0' + (i % 10));
+		putchar(' ');
+		putchar('0' + (j / 10));
+		putchar('0' + (j % 10));
+	if (i != 99 || j != 99)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
 	}
 
 	return 0;
