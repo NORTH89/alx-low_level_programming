5c9e2b3ae14f3161c828cde04da7447804421414
NOUREDDINE <nordintata0@gmail.com>
2023-09-12 14:53:48 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _abs - absolute value
+ * @i: integer
+ *
+ * Return: return absolute valuer of a number
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+		return (n);
+	}
+	else
+	{
+		return (i);
+	}
+}
