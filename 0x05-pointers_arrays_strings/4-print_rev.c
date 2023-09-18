a6ed47c9e3c43f880e5326a5a0a7d62722aa00b9
NOUREDDINE <nordintat0@gmail.com>
2023-09-18 20:55:48 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: variable int
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int r = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		r++;
+	}
+	s--;
+	r--;
+
+	while (r >= 0)
+	{
+		_putchar(*s);
+		s--;
+		r--;
+	}
+	_putchar('\n');
+}
