302f1d2d302ed4b5278207dd3b3852602e8953af
NOUREDDINE <nordintat0@gmail.com>
2023-09-21 12:02:16 +0100

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - function that encodes a string
+ * @str: string to be encoded
+ * Return: pointer to encoded string
+ */
+char *rot13(char *str)
+{
+	char *start = str;
+	int diff;
+
+	while (*str != '\0')
+	{
+		diff = (*str >= 'a') ? ('a') : ('A');
+		if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
+		{
+			*str = ((*str - diff + 13) % 26) + diff;
+		}
+		str++;
+	}
+
+	return (start);
+}
