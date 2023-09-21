6bbd9fe8f1d284adddb57c798b9351f9e6588250
NOUREDDINE <nordintat0@gmail.com>
2023-09-21 12:50:11 +0100

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,27 +12,10 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n == 0)
+	if (n / 10)
 	{
-		_putchar('0');
-		return;
+		print_number(n / 10);
 	}
 
-	int divisor = 1;
-	int temp = n;
-
-	while (temp != 0)
-	{
-		temp /= 10;
-		divisor *= 10;
-	}
-
-	divisor /= 10;
-
-	while (divisor != 0)
-	{
-		_putchar((n / divisor) + '0');
-		n %= divisor;
-		divisor /= 10;
-	}
+	_putchar((n % 10) + '0');
 }
