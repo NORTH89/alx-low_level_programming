d5c1b75772ba5dfbbb64b14b205d6c44ccdd1987
NOUREDDINE <nordintata0@gmail.com>
2023-09-25 15:31:37 +0100

    for_all

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,26 @@
 #include "main.h"
-
+#include <stddef.h>
 /**
- * _strchr - locates a character in a string
- * @s: the string to search
- * @c: the character to locate
- *
- * Return: a pointer to the first occurrence of the character in the string,
- *         or NULL if the character is not found
+ * _strchr - locates a character in a string.
+ * Description: returns a point to the first occurance of the char c.
+ * @s: string to be searched through.
+ * @c: charater to be looked for.
+ * Return: returns s or returns NULL.
  */
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
 		if (*s == c)
+		{
 			return (s);
+		}
+		else if (*(s + 1) == c)
+		{
+			return (s + 1);
+		}
 		s++;
 	}
-
-	return (0);
+	return (s + 1);
+	return (NULL);
 }
