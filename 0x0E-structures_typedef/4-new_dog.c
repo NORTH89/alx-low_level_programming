a46f1cddb26c14f784839b49282e5b0bce095dff
NOUREDDINE <nordintata0@gmail.com>
2023-10-10 19:15:54 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include "dog.h"
 
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
