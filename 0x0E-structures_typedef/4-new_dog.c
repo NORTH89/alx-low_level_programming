d1d05e101a815ad90ea6cfc28bd11936e5f15c0c
NOUREDDINE <nordintata0@gmail.com>
2023-10-10 19:14:35 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,14 +3,6 @@
 #include <string.h>
 #include "dog.h"
 
-/**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
@@ -21,20 +13,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = strdup(name);
+	new_dog->name = malloc(strlen(name) + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+	strcpy(new_dog->name, name);
 
-	new_dog->owner = strdup(owner);
+	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
+	strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
 
