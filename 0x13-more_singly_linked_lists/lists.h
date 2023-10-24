b6ca87356aff577563bfba6393d8c6a2d3933d5b
NOUREDDINE <nordintata0@gmail.com>
2023-10-24 11:30:01 +0100

    4-free_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 /*data structure for singly linked list*/
 #include <stddef.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -21,5 +21,9 @@ typedef struct listint_s
 
 /** Function that prints all the elements of a listint_t list */
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 #endif /* lists.h */
