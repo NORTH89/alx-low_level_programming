8b3c42cae9d489a727bd60d3d3403832a9957277
NOUREDDINE <nordintata0@gmail.com>
2023-10-24 14:13:19 +0100

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,10 +12,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 
 	if (head == NULL || index >= (unsigned int)head->n)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < index; i++)
 		current = current->next;
 
-	return current;
+	return (current);
 }
