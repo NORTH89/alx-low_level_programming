994d95a9852994745937e8929f0ba7d23a72fa48
NOUREDDINE <nordintata0@gmail.com>
2023-10-24 14:12:24 +0100

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
+
 } listint_t;
 
 /** Function that prints all the elements of a listint_t list */
@@ -27,4 +28,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
 #endif /* lists.h */
