2c8f2ce8b2eef6a7677ff0e61ca981b4f6cac6db
NOUREDDINE <nordintata0@gmail.com>
2023-10-24 12:20:28 +0100

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,18 +7,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (*head != NULL)
+	listint_t *temp, *current;
+
+	if (head == NULL)
+		return;
+	current = *head;
+	while (current != NULL)
 	{
-		listint_t *current = *head;
-
-		while (current != NULL)
-		{
-			listint_t *temp = current;
-
-			current = current->next;
-			free(temp);
-		}
-
-		*head = NULL;
+		temp = current;
+		current = current->next;
+		free(temp);
 	}
+	*head = NULL;
 }
