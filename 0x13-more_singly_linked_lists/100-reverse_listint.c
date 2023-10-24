cac385acd1a22eeb560f3e9429f25e8f76e01e9c
NOUREDDINE <nordintata0@gmail.com>
2023-10-24 23:04:04 +0100

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,21 +6,8 @@
  *
  * Return: the address of the new head.
  */
-
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-
-	while (current != NULL)
-	{
-		listint_t *next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-
-	return *head;
+	head++;
+	return (0);
 }
