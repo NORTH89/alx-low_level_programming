0ff161e2f5e7834b6786bc2c258ebbdde84d93ff
NOUREDDINE <nordintata0@gmail.com>
2023-10-30 19:04:18 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,13 +4,6 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: the number of letters it should read and print
- *
- * Return: the number of letters it could read and print
- */
-/**
- * read_textfile - reads a text file and prints it to the POSIX standard output
  *
  * @filename: name of text file
  * @letters: number of letters it should read and print.
