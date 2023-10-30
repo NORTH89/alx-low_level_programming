98cf1c449797bfdb0ed36ab21ff71723b8b8f2dd
NOUREDDINE <nordintata0@gmail.com>
2023-10-30 18:59:28 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,39 +9,46 @@
  *
  * Return: the number of letters it could read and print
  */
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: name of text file
+ * @letters: number of letters it should read and print.
+ *
+ * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
-	char *buffer;
-	size_t bytes_read;
-	ssize_t total_chars = 0;
+	ssize_t da, wr, re;
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
 
-	file = fopen(filename, "r");
-	if (file == NULL)
+	buff = malloc(letters);
+	if (buff == NULL)
 		return (0);
 
-	buffer = malloc(letters + 1);
-	if (buffer == NULL)
-	{
-		fclose(file);
+	da = open(filename, O_RDWR);
+
+	if (da == -1)
 		return (0);
-	}
 
-	while ((bytes_read = fread(buffer, sizeof(char), letters, file)) > 0)
-	{
-		if (fwrite(buffer, sizeof(char), bytes_read, stdout) != bytes_read)
-		{
-			free(buffer);
-			fclose(file);
-			return (0);
-		}
-		total_chars += bytes_read;
-	}
+	re = read(da, buff, letters);
+	if (re == -1)
+		return (0);
 
-	free(buffer);
-	fclose(file);
-	return (total_chars);
+	wr = write(STDOUT_FILENO, buff, re);
+	if (wr == -1 || wr != re)
+		return (0);
+
+	re = close(da);
+	if (re == -1)
+		return (0);
+
+	free(buff);
+	return (wr);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stddef.h>
-
+#include <fcntl.h>
 /*prototypes*/
 ssize_t read_textfile(const char *filename, size_t letters);
 
