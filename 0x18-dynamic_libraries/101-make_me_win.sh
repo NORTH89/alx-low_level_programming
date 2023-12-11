8993bc2b04af01ecca490bf10106249352467f51
NORTH89 <nordintata0@gmail.com>
2023-12-11 20:57:36 +0100

    file.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/NOUREDDINE/alx-low_level_programming/blob/532272e2c713cf78e9e54d816e0e890353d3684e/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/NOUREDDINE/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
