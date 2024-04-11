3230558e8259796ae5f7d78455e9c7a8ded7d9d5
NORTH89 <nordintata0@gmail.com>
2024-04-11 09:15:28 +0000

    update code

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,18 +1,13 @@
 #include "search_algos.h"
 
 /**
- * jump_search - Searches for a value in a sorted array
- *               of integers using jump search.
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
- * @value: The value to search for.
- *
- * Return: If the value is not present or the array is NULL, -1.
- *         else, the first index where the value is located.
- *
- * Description: Prints a value every time it is compared in the array.
- *              Uses the square root of the array size as the jump step.
+ * jump_search - linear search algorithm
+ * @array: A pointer to the first element of the array
+ * @size: The size of the array
+ * @value: The value to search for
+ * Return: The index where value is located, or -1
  */
+
 int jump_search(int *array, size_t size, int value)
 {
 	size_t i, jump, step;
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,6 @@ void print_array(int *array, size_t size);
 int _advanced_binary(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
 size_t min(size_t a, size_t b);
+int interpolation_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 #endif
